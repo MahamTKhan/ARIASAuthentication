5d485d56dd88864b96b433b87f45ed07e4b209c6
hamna4503 <hamnaaamir593@gmail.com>
2024-10-02 19:59:25 +0500

    Initialized Project Structure
    Signed-off-by: hamna4503 <hamnaaamir593@gmail.com>

---
+++ src/config/alerts/authMessages.js
@@ -0,0 +1,8 @@
+const authStatusMessages = {
+  401: "Unauthorized - Please log in to continue.",
+  403: "Access Denied - You do not have permission to view this resource.",
+};
+
+export default authStatusMessages;
+
+//THIS IS JUST TO PROVIDE AN IDEA
